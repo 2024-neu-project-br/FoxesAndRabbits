66f079695e37e6e692b7536a331df48f1f315a92
brown-bas <barnusfutureonline@gmail.com>
2024-10-19 15:58:38 +0200

    Added some basic UI and extra functionality

--- FoxesAndRabbits/FAR/Display/WebPage/app.js
+++ FoxesAndRabbits/FAR/Display/WebPage/app.js
@@ -1,10 +1,14 @@
 /* --- Define basic variables and values --- */
 const game = document.getElementById("game");
-const tickInterval = document.getElementById("tickInterval").value;
-const gameInstanceName = document.getElementById("gameInstanceName").value;
+const tickInterval = !document.getElementById("tickInterval").value ? 10 : document.getElementById("tickInterval").value;
+const gameInstanceName = !document.getElementById("gameInstanceName").value ? "FAR" : document.getElementById("gameInstanceName").value;
 const pauseGame = document.getElementById("pauseGame");
 const newGame = document.getElementById("newGame");
-pauseGame.toggleAttribute("disabled");
+const addFox = document.getElementById("addFox");
+const addRabbit = document.getElementById("addRabbit");
+
+// There's no instance yet, so you can't do much ¯\_(ツ)_/¯
+[pauseGame, addFox, addRabbit].forEach(element => element.toggleAttribute("disabled"));
 
 pauseGame.onclick = () => {
     // This checks whether the game is playing or not and toggles that attribute based on that
@@ -12,16 +16,18 @@ pauseGame.onclick = () => {
     else game.setAttribute("playing", "");
 
     // Updating the buttons
-    newGame.toggleAttribute("disabled");
+    [newGame, addFox, addRabbit].forEach(element => element.toggleAttribute("disabled"))
     pauseGame.innerText = getAttr(game, "playing") ? "Pause game" : "Unpause game";
-    
+
+    handleResponse(command("T"));
+
     /* THIS IS FOR TESTING PURPOSES ONLY, IT WILL BE REMOVED IN THE FINAL RELEASE */
     console.log(`Game paused!\nGame started: ${getAttr(game, "started")}\nGame playing: ${getAttr(game, "playing")}`);
 }
 
 newGame.onclick = () => {
-    // Checks if the pause button is disabled or not, enables it if yes
-    if(getAttr(pauseGame, "disabled")) pauseGame.removeAttribute("disabled");
+    // Checks if these buttons are disabled or not, enables them if yes
+    [pauseGame, addFox, addRabbit].forEach(element => {if(getAttr(element, "disabled")) element.removeAttribute("disabled");});
 
     // Setting basic attributes and updating the pause button
     game.setAttribute("started", "");
@@ -31,10 +37,15 @@ newGame.onclick = () => {
     // Disables the new game button until the game gets paused
     newGame.toggleAttribute("disabled");
     
+    handleResponse(newInstance());
+
     /* THIS IS FOR TESTING PURPOSES ONLY, IT WILL BE REMOVED IN THE FINAL RELEASE */
     console.log(`New game started!\nGame started: ${getAttr(game, "started")}\nGame playing: ${getAttr(game, "playing")}`);
 }
 
+// Spawns an entity on the map upon clicking the button
+addFox.onclick = () => handleResponse(command("+E")/* Fox */)
+addRabbit.onclick = () => handleResponse(command("+E")/* Rabbit */)
 
 /* This function just helps shortening the code a bit */
 function getAttr(element, attribute){
@@ -52,4 +63,9 @@ function parseMap(mapString){
     map.mobMap = lines.slice(lines.findIndex(x=>x=="MOBMAP")+1, lines.length-1);
     
     return map;
+}
+
+/* This doesn't do anything yet, just console.logs the response, later it will actually handle the response it gets */
+function handleResponse(response){
+    console.log(response);
 }
