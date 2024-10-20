07d10b79c42aa7ff0fa5b0270446ee6986f46258
brown-bas <barnusfutureonline@gmail.com>
2024-10-20 03:09:38 +0200

    Please just end me

--- FoxesAndRabbits/FAR/Entities/Rabbit.cs
+++ FoxesAndRabbits/FAR/Entities/Rabbit.cs
@@ -20,9 +20,35 @@ namespace FoxesAndRabbits.FAR.Entities {
         public override void IndividualUpdate() {
 
             bool canUwU = foodLevel == maxFoodLevel || instance.map.GetEntitiesAround(X, Y).Count == 0; // huh? rabbits cant pounce according to the game rules, im not sure what you did here
-
-            
         
+        }
+        public void Eat()
+        {
+            switch (instance.map.grassMap[X, Y]) {
+                case 2:
+                    if (foodLevel != maxFoodLevel)
+                    {
+                        foodLevelNew++;
+                        instance.map.grassMapNew[X, Y] = 0;
+                    }
+                break;
+                case 3:
+                    if (foodLevel != maxFoodLevel - 1) {
+                        foodLevelNew++;
+                        instance.map.grassMapNew[X, Y] = 0;
+                    }
+                    if (foodLevel != maxFoodLevel) foodLevelNew++;
+                break;
+            }
+        }
+
+
+        public void Move()
+        {
+            int[] randomCell = instance.map.GetRandomEmptyCellAround(X, Y, 1);
+            X = randomCell[0];
+            Y = randomCell[1];
+            // I know I'm probably not doing this right, but please just leave this be for the time being, because I actually feel like I'll just kms after making the website part work
         }
 
     }
